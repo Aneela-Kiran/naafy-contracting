1afb7dd24dec1313ecf654b9cce7a5e03af4abd3
HamzaTahir-codes <m.hamza.codes@gmail.com>
2024-09-28 15:22:27 +0500

    Forgot Password Disabled

--- useradmin/urls.py
+++ useradmin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 app_name = "useradmin"
 
@@ -69,8 +69,8 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("change-password/", views.change_password, name="change-password"),
-    path('admin/password_reset/',auth_views.PasswordResetView.as_view(),name='admin_password_reset'),
-    path('admin/password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+#     path('admin/password_reset/',auth_views.PasswordResetView.as_view(),name='admin_password_reset'),
+#     path('admin/password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+#     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+#     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
